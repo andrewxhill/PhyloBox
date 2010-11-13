cba57d07705cd41d1069a017591e324e0165ad01
plebeian <sanderpick@gmail.com>
2010-11-13 15:55:27 -0800

    code formatting

--- static/js/2-0/main.phylobox.js
+++ static/js/2-0/main.phylobox.js
@@ -1251,7 +1251,7 @@ var PhyloBox = function(phylobox_container_div_id, phylobox_environment_options)
                 this._data.environment.branchlabels = pB.Options.branchLabels == null ? this._data.environment.branchlabels : pB.Options.branchLabels;
                 this._data.environment.threeD = pB.Options.threeD == null ? this._data.environment.threeD : pB.Options.threeD;
                 this._title = pB.Options.title == true ? this._title : pB.Options.title;
-                        console.log(this._data.environment.width);
+                
                 switch(pB.Options.viewMode){
                     case "dendrogram":
                         this._data.environment.viewmode = 0;
@@ -1267,9 +1267,6 @@ var PhyloBox = function(phylobox_container_div_id, phylobox_environment_options)
                         this._data.environment.viewmode = 3;
                         break;
                 }
-                    
-                
-                //console.log(this._title);
             }
 		},
 		save:function() {
